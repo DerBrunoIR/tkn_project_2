3a053debb3fa29c0760a38bc65e7589da6388860
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2022-12-27 18:18:17 +0100

    hash_table.c is now compilable

--- praxis2/src/hash_table.c
+++ praxis2/src/hash_table.c
@@ -8,9 +8,13 @@ void htable_set(htable **ht, const unsigned char *key, size_t key_len,
 htable *htable_get(htable **ht, const unsigned char *key, size_t key_len) {
     /* TODO IMPLEMENT */
     // return NULL on error
+    return NULL;
 }
 
 int htable_delete(htable **ht, const unsigned char *key, size_t key_len) {
     /* TODO IMPLEMENT */
     // return -1 on error
+    htable* item = malloc(sizeof(htable));
+    HASH_FIND(hh, *ht, key, key_len , item);
+    return -1;
 }
