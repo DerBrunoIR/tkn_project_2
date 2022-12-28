8b9ee2209b815e1d5d40b3555b312ab628538c93
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2022-12-28 17:28:04 +0100

    added hast_table test

---
+++ praxis2/my_tests/test_hash_table.c
@@ -0,0 +1,91 @@
+#include "hash_table.h"
+#include <assert.h>
+#include <stdio.h>
+
+
+htable** create_htable() {
+	htable** ht = malloc(sizeof(htable*));
+	*ht = NULL;
+	return ht;
+}
+
+htable* item_new(char* key, int key_len, char* value, int value_len) {
+	htable* ht = malloc(sizeof(htable));
+	ht->value_len = value_len;
+	ht->key_len = key_len;
+	strncpy((char*) ht->key, key, key_len);
+	strncpy((char*) ht->value, value, value_len);
+	return ht;
+}
+
+void item_free(htable* item) {
+	if (item->key != NULL) {
+		free(item->key);
+	}
+	if (item->value != NULL) {
+		free(item->value);
+	}
+	free(item);
+}
+
+void assertItem(htable* item, char* key, int key_len, char* val, int val_len) {
+	assert(item->key_len == key_len);
+	assert(item->value_len == val_len);
+	assert(strncmp((char*) item->key, key, key_len));
+	assert(strncmp((char*) item->value, val, val_len));
+}
+
+int main() {
+	// setup
+	htable** ht = create_htable();
+	char key1[] = "key1";
+	char key2[] = "key2";
+	char key3[] = "key3";
+	int key1len = strlen(key1);
+	int key2len = strlen(key2);
+	int key3len = strlen(key3);
+	char val1[] = "val1";
+	char val2[] = "val2";
+	char val3[] = "val3";
+	int val1len = strlen(val1);
+	int val2len = strlen(val2);
+	int val3len = strlen(val3);
+
+	// test 1
+	printf("Test %d \n", 1);
+	htable_set(ht, key1, key1len, val1, val1len);
+	assertItem(htable_get(ht, key1, key1len), key1, key1len, val1, val1len);
+	assert(htable_get(ht, key2, key2len) == NULL);
+	assert(htable_get(ht, key3, key3len) == NULL);
+
+	// test 2, add value
+	printf("Test %d \n", 2);
+	htable_set(ht, key2, key2len, val2, val2len);
+	assertItem(htable_get(ht, key1, key1len), key1, key1len, val1, val1len);
+	assertItem(htable_get(ht, key2, key2len), key2, key2len, val2, val2len);
+	assert(htable_get(ht, key3, key3len) == NULL);
+
+	// test 3, add value
+	printf("Test %d \n", 3);
+	htable_set(ht, key3, key3len, val3, val3len);
+	assertItem(htable_get(ht, key1, key1len), key1, key1len, val1, val1len);
+	assertItem(htable_get(ht, key2, key2len), key2, key2len, val2, val2len);
+	assertItem(htable_get(ht, key3, key3len), key3, key3len, val3, val3len);
+
+	// test 4, delte value
+	printf("Test %d \n", 4);
+	htable_delete(ht, key1, key1len);
+	assert(htable_get(ht, key1, key1len) == NULL);
+	assertItem(htable_get(ht, key2, key2len), key2, key2len, val2, val2len);
+	assertItem(htable_get(ht, key3, key3len), key3, key3len, val3, val3len);
+
+	// test 5, set value alread in ht
+	printf("Test %d \n", 5);
+	htable_set(ht, key3, key3len, val2, val2len);
+	assert(htable_get(ht, key1, key1len) == NULL);
+	assertItem(htable_get(ht, key2, key2len), key2, key2len, val2, val2len);
+	assertItem(htable_get(ht, key3, key3len), key3, key3len, val2, val2len);
+	
+	printf("Done, all tests successfull");
+	return 0;
+}
