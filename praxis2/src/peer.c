5374bb09eea24c1e882ca53158ca7a70e559cd63
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2022-12-31 18:00:35 +0100

    ein weitere Schritt

--- praxis2/src/peer.c
+++ praxis2/src/peer.c
@@ -47,7 +47,7 @@ void packet_network_byte_order(packet* p) {
 }
 
 int send_packet(int socket, packet* pkt) {
-	fprintf(stderr, "sending packet to socket %d\n", socket);
+	fprintf(stderr, "sending packet\n");
 	if (socket < 1) {
 		fprintf(stderr, "peer.c::send_packet::received closed socket!\n");
 		return -1;
@@ -66,13 +66,13 @@ int send_packet(int socket, packet* pkt) {
 /**
  * @brief Forward a packet to a peer.
  *
- * @param peer The peer to forward the request to. The socket is created via peer->hostname
+ * @param peer The peer to forward the request to. 
  * @param pack The packet to forward
  * @return int The status of the sending procedure
  */
 int forward(peer *p, packet *pack) {
-	fprintf(stderr, "forwarding packet to peer %s\n", p->hostname);
 	/* DONE (Bruno) */
+	fprintf(stderr, "forwarding packet to peer %d\n", p->node_id);
 	peer_connect(p);
 	send_packet(p->socket, pack); 
 	peer_disconnect(p);
@@ -89,21 +89,27 @@ int forward(peer *p, packet *pack) {
  * @return int The callback status
  */
 int proxy_request(server *srv, int csocket, packet *p, peer *n) {
-	fprintf(stderr, "sending proxy request to %s\n", n->hostname);
 	/* DONE (Bruno) */
-	
-	// do request and receive response
+	fprintf(stderr, "sending proxy request to %s:%d\n", n->hostname, n->port);
+	// send request
 	peer_connect(n);
 	send_packet(n->socket, p);
-	
+
+	fprintf(stderr, "waiting for data...\n");
 	size_t pkt_buf_size = 0;
 	unsigned char* pkt_buf = recvall(n->socket, &pkt_buf_size);
 	peer_disconnect(n);
+	fprintf(stderr, "received %lu bytes\n", pkt_buf_size);
 
-	// proxy response 
-	sendall(csocket, pkt_buf, pkt_buf_size);
+	packet* prxyp = packet_decode(pkt_buf, pkt_buf_size);
+
+	// send response 
+	fprintf(stderr, "send response back to origin\n");
+	int status = sendall(csocket, pkt_buf, pkt_buf_size);
+	server_close_socket(srv, csocket);
+	fprintf(stderr, "status=%d\n", status);
 	free(pkt_buf);
-	
+
 	return CB_OK;
 }
 
@@ -114,8 +120,8 @@ int proxy_request(server *srv, int csocket, packet *p, peer *n) {
  * @return int The callback status
  */
 int lookup_peer(uint16_t hash_id) {
-	fprintf(stderr, "lookup peer responsible for %d\n", hash_id);
 	/* DONE (Bruno) */
+	fprintf(stderr, "lookup peer responsible for %d\n", hash_id);
 	// build lookup packet
 	packet* pkt = packet_new();
 	pkt->flags     = PKT_FLAG_CTRL | PKT_FLAG_LKUP;
@@ -143,35 +149,39 @@ int lookup_peer(uint16_t hash_id) {
  * @return int The callback status
  */
 int handle_own_request(server *srv, client *c, packet *pkt_rcvd) {
-	fprintf(stderr, "handle own request");
 	/* TOTEST (Bruno) */
+	fprintf(stderr, "handle own request\n");
 	// build response packet 
-	packet* pkt_snd = packet_new();
-	pkt_snd->value_len = 0;
-	pkt_snd->key_len = pkt_rcvd->key_len;
-	pkt_snd->key = malloc(pkt_rcvd->key_len * sizeof(char));
-	strncpy(pkt_snd->key, pkt_rcvd->key, pkt_rcvd->key_len);
+	packet* pkt_snd = packet_dup(pkt_rcvd);
+	pkt_snd->flags = PKT_FLAG_ACK | PKT_FLAG_RPLY;
 
 	if (pkt_rcvd->flags & PKT_FLAG_GET) {
-		pkt_snd->flags = PKT_FLAG_RPLY;
+		fprintf(stderr, "GET %s\n", pkt_rcvd->key);
 		htable* item = htable_get(ht, pkt_rcvd->key, pkt_rcvd->key_len);
 		if (item) {
+			fprintf(stderr, "ITEM found: %s\n", pkt_rcvd->value);
+			free(pkt_snd->value);
+			pkt_snd->value_len = item->value_len;
 			pkt_snd->value = malloc(item->value_len * sizeof(char));
 			strncpy(pkt_snd->value, item->value, item->value_len);
-			pkt_snd->value_len = item->value_len;
-		} 
+		} else {
+			fprintf(stderr, "ITEM not found\n");
+		}
 	}
 	else if (pkt_rcvd->flags & PKT_FLAG_SET) {
+		fprintf(stderr, "SET %s\n", pkt_rcvd->key);
 		htable_set(ht, pkt_rcvd->key, pkt_rcvd->key_len, pkt_rcvd->value, pkt_rcvd->value_len);
-		pkt_snd->flags = PKT_FLAG_ACK;
 	} 
 	else if (pkt_rcvd->flags & PKT_FLAG_DEL) {
+		fprintf(stderr, "DEL %s\n", pkt_rcvd->key);
 		htable_delete(ht, pkt_rcvd->key, pkt_rcvd->key_len);
-		pkt_snd->flags = PKT_FLAG_ACK;
+	} else {
+		fprintf(stderr, "UNKOWN METHOD %s\n", pkt_rcvd->key);
 	}
 
 	// send response 
 	send_packet(c->socket, pkt_snd);
+	server_close_socket(srv, c->socket);
 
 	packet_free(pkt_snd);
 	return CB_OK;
@@ -185,22 +195,23 @@ int handle_own_request(server *srv, client *c, packet *pkt_rcvd) {
  * @return int The callback status
  */
 int answer_lookup(packet *pkt_rcvd, peer *p) {
-	fprintf(stderr, "answer lookup from peer %s\n", p->hostname);
 	/* DONE (Bruno) */
-	// build response packet
 	peer* n = peer_from_packet(pkt_rcvd);
+	fprintf(stderr, "answer lookup from peer %d\n", n->node_id);
+	// build response packet
 	packet* pkt_snd = packet_new();
 	pkt_snd->flags     = PKT_FLAG_CTRL | PKT_FLAG_RPLY;
-	pkt_snd->hash_id   = pkt_rcvd->hash_id;  // requested hash
-	pkt_snd->node_id   = p->node_id; // node_id who can resolve the given hash
-	pkt_snd->node_port = p->port;    // port
+	pkt_snd->hash_id   = pkt_rcvd->hash_id;
+	pkt_snd->node_id   = p->node_id;
+	pkt_snd->node_port = p->port;
 	pkt_snd->node_ip   = peer_get_ip(p);
-	
+
 	// send response to packet origin
 	peer_connect(n);
 	send_packet(n->socket, pkt_snd);
 	peer_disconnect(n);
 
+	peer_free(n);
 	packet_free(pkt_snd);
 	return 0;
 }
@@ -215,26 +226,26 @@ int answer_lookup(packet *pkt_rcvd, peer *p) {
  */
 int handle_packet_data(server *srv, client *c, packet *p) {
 	fprintf(stderr, "DATA\n");
-    // Hash the key of the <key, value> pair to use for the hash table
-    uint16_t hash_id = pseudo_hash(p->key, p->key_len);
-    fprintf(stderr, "Hash id: %d\n", hash_id);
+	// Hash the key of the <key, value> pair to use for the hash table
+	uint16_t hash_id = pseudo_hash(p->key, p->key_len);
+	fprintf(stderr, "Hash id: %d from key %s\n", hash_id, p->key);
 
-    // Forward the packet to the correct peer
-    if (peer_is_responsible(pred->node_id, self->node_id, hash_id)) {
-        // We are responsible for this key
-        fprintf(stderr, "This node can answer the request.\n");
-        return handle_own_request(srv, c, p);
-    } else if (peer_is_responsible(self->node_id, succ->node_id, hash_id)) {
-        // Our successor is responsible for this key
-        fprintf(stderr, "The successor can answer the request.\n");
-        return proxy_request(srv, c->socket, p, succ);
-    } else {
-        // We need to find the peer responsible for this key
-        fprintf(stderr, "Some other node can answer the request.\n");
-        add_request(rt, hash_id, c->socket, p);
-        lookup_peer(hash_id); // TODO Add this to open lookup requests
-        return CB_OK;
-    }
+	// Forward the packet to the correct peer
+	if (peer_is_responsible(pred->node_id, self->node_id, hash_id)) {
+		// We are responsible for this key
+		fprintf(stderr, "This node can answer the request.\n");
+		return handle_own_request(srv, c, p);
+	} else if (peer_is_responsible(self->node_id, succ->node_id, hash_id)) {
+		// Our successor is responsible for this key
+		fprintf(stderr, "The successor can answer the request.\n");
+		return proxy_request(srv, c->socket, p, succ);
+	} else {
+		// We need to find the peer responsible for this key
+		fprintf(stderr, "Some other node can answer the request.\n");
+		add_request(rt, hash_id, c->socket, p);
+		lookup_peer(hash_id); // TODO Add this to open lookup requests
+		return CB_OK;
+	}
 }
 
 /**
@@ -248,35 +259,35 @@ int handle_packet_data(server *srv, client *c, packet *p) {
  */
 int handle_packet_ctrl(server *srv, client *c, packet *p) {
 	fprintf(stderr, "CTRL\n");
-    if (p->flags & PKT_FLAG_LKUP) {
-	fprintf(stderr, "LKUP for hash_id %d\n", p->hash_id);
-        // we received a lookup request
-        if (peer_is_responsible(pred->node_id, self->node_id, p->hash_id)) {
-            // Our business
-            fprintf(stderr, "This node is responsible (should not happen!).\n");
-            return answer_lookup(p, self);
-        } else if (peer_is_responsible(self->node_id, succ->node_id,
-                                       p->hash_id)) {
-            fprintf(stderr, "The successor is responsible.\n");
-            return answer_lookup(p, succ);
-        } else {
-            // Great! Somebody else's job!
-	    fprintf(stderr, "Some other node is responsible.\n");
-            forward(succ, p);
-        }
-    } else if (p->flags & PKT_FLAG_RPLY) {
-        // Look for open requests and proxy them
-        peer *n = peer_from_packet(p);
-	fprintf(stderr, "Received lookup answer from node %s:%d.\n", n->hostname, n->port);
-        for (request *r = get_requests(rt, p->hash_id); r != NULL;
-             r = r->next) {
-            proxy_request(srv, r->socket, r->packet, n);
-            server_close_socket(srv, r->socket);
-        }
-        clear_requests(rt, p->hash_id);
-    } else {
-    }
-    return CB_REMOVE_CLIENT;
+	if (p->flags & PKT_FLAG_LKUP) {
+		fprintf(stderr, "LKUP for hash_id %d\n", p->hash_id);
+		// we received a lookup request
+		if (peer_is_responsible(pred->node_id, self->node_id, p->hash_id)) {
+			// Our business
+			fprintf(stderr, "This node is responsible (should not happen!).\n");
+			return answer_lookup(p, self);
+		} else if (peer_is_responsible(self->node_id, succ->node_id,
+					p->hash_id)) {
+			fprintf(stderr, "The successor is responsible.\n");
+			return answer_lookup(p, succ);
+		} else {
+			// Great! Somebody else's job!
+			fprintf(stderr, "Some other node is responsible.\n");
+			forward(succ, p);
+		}
+	} else if (p->flags & PKT_FLAG_RPLY) {
+		// Look for open requests and proxy them
+		peer *n = peer_from_packet(p);
+		fprintf(stderr, "Received lookup answer.\n");
+		for (request *r = get_requests(rt, p->hash_id); r != NULL;
+				r = r->next) {
+			proxy_request(srv, r->socket, r->packet, n);
+			server_close_socket(srv, r->socket);
+		}
+		clear_requests(rt, p->hash_id);
+	} else {
+	}
+	return CB_REMOVE_CLIENT;
 }
 
 /**
@@ -290,7 +301,7 @@ int handle_packet_ctrl(server *srv, client *c, packet *p) {
  * @return int The callback status
  */
 int handle_packet(server *srv, client *c, packet *p) {
-	fprintf(stderr, "Received packet\n");
+	fprintf(stderr, "Received packet from %d\n", p->node_id);
 	if (p->flags & PKT_FLAG_CTRL) {
 		return handle_packet_ctrl(srv, c, p);
 	} else {
@@ -317,51 +328,53 @@ int handle_packet(server *srv, client *c, packet *p) {
  * @return int The exit code
  */
 int main(int argc, char **argv) {
+	if (argc < 10) {
+		fprintf(stderr, "Not enough args! I need ID IP PORT ID_P IP_P PORT_P "
+				"ID_S IP_S PORT_S\n");
+	}
 
-    if (argc < 10) {
-        fprintf(stderr, "Not enough args! I need ID IP PORT ID_P IP_P PORT_P "
-                        "ID_S IP_S PORT_S\n");
-    }
+	// Read arguments for self
+	uint16_t idSelf = strtoul(argv[1], NULL, 10);
+	char *hostSelf = argv[2];
+	char *portSelf = argv[3];
 
-    // Read arguments for self
-    uint16_t idSelf = strtoul(argv[1], NULL, 10);
-    char *hostSelf = argv[2];
-    char *portSelf = argv[3];
+	// Read arguments for predecessor
+	uint16_t idPred = strtoul(argv[4], NULL, 10);
+	char *hostPred = argv[5];
+	char *portPred = argv[6];
 
-    // Read arguments for predecessor
-    uint16_t idPred = strtoul(argv[4], NULL, 10);
-    char *hostPred = argv[5];
-    char *portPred = argv[6];
+	// Read arguments for successor
+	uint16_t idSucc = strtoul(argv[7], NULL, 10);
+	char *hostSucc = argv[8];
+	char *portSucc = argv[9];
 
-    // Read arguments for successor
-    uint16_t idSucc = strtoul(argv[7], NULL, 10);
-    char *hostSucc = argv[8];
-    char *portSucc = argv[9];
+	// Initialize all chord peers
+	self = peer_init(
+			idSelf, hostSelf,
+			portSelf); //  Not really necessary but convenient to store us as a peer
+	pred = peer_init(idPred, hostPred, portPred); //
 
-    // Initialize all chord peers
-    self = peer_init(
-        idSelf, hostSelf,
-        portSelf); //  Not really necessary but convenient to store us as a peer
-    pred = peer_init(idPred, hostPred, portPred); //
+	succ = peer_init(idSucc, hostSucc, portSucc);
 
-    succ = peer_init(idSucc, hostSucc, portSucc);
+	// Initialize outer server for communication with clients
+	server *srv = server_setup(portSelf);
+	if (srv == NULL) {
+		fprintf(stderr, "Server setup failed!\n");
+		return -1;
+	}
+	// Initialize hash table
+	ht = (htable **)malloc(sizeof(htable *));
+	// Initiale reuqest table
+	rt = (rtable **)malloc(sizeof(rtable *));
+	*ht = NULL;
+	*rt = NULL;
 
-    // Initialize outer server for communication with clients
-    server *srv = server_setup(portSelf);
-    if (srv == NULL) {
-        fprintf(stderr, "Server setup failed!\n");
-        return -1;
-    }
-    // Initialize hash table
-    ht = (htable **)malloc(sizeof(htable *));
-    // Initiale reuqest table
-    rt = (rtable **)malloc(sizeof(rtable *));
-    *ht = NULL;
-    *rt = NULL;
+	fprintf(stderr, "My Hostname: %s:%d\n", self->hostname, self->port);
+	fprintf(stderr, "My Node Id: %d\n", self->node_id);
+	fprintf(stderr, "My Predecessor Id: %d\n", pred->node_id);
+	fprintf(stderr, "My Successor Id: %d\n", succ->node_id);
 
-    fprintf(stderr, "My Hostname: %s:%d\n", self->hostname, self->port);
-    fprintf(stderr, "My Hash Id: %d\n", self->node_id);
-    srv->packet_cb = handle_packet;
-    server_run(srv);
-    close(srv->socket);
+	srv->packet_cb = handle_packet;
+	server_run(srv);
+	close(srv->socket);
 }
